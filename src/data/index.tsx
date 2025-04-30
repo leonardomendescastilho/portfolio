2f4ff227b8f0cd99dd31a3133da1a8abff16444c
Leonardo Mendes Castilho <leonardo.mendescastilho@gmail.com>
2025-04-30 11:39:53 -0300

    feat: Implementa estrutura de dados e definições de tipo

---
+++ src/data/index.tsx
@@ -0,0 +1,13 @@
+import movieFinderBg from "@/assets/moviefinder-bg.webp";
+
+export const projects = [
+  {
+    id: '1',
+    title: "MovieFinder",
+    description:
+      "Movie Finder é um site onde os usuários podem pesquisar filmes pelo nome em inglês, visualizar detalhes sobre eles e salvar seus filmes favoritos para consulta futura.", 
+    backgroundImage: movieFinderBg,
+    TechStack: ["React", "TypeScript", "Tailwind"],
+    linkUrl: "https://leonardomendescastilho.github.io/MovieFinder/",
+  }
+]
---
+++ src/types/index.ts
@@ -0,0 +1,8 @@
+export interface IProject {
+  id: string;
+  title: string;
+  description: string;
+  backgroundImage: string;
+  TechStack: string[];
+  linkUrl: string;
+}
