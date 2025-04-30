180744aa2acaf4b83cdcd4f451d63f9d5362c4d6
Leonardo Mendes Castilho <leonardo.mendescastilho@gmail.com>
2025-04-30 14:22:15 -0300

    style: Ajusta as colunas do grid

--- src/components/home/TechStack.tsx
+++ src/components/home/TechStack.tsx
@@ -3,12 +3,11 @@ import { Badge } from '../ui/badge';
 import { motion } from 'motion/react';
 
 const AnimatedBadge = motion(Badge);
-
 export const TechStack = () => {
 	return (
 		<section className='flex items-center justify-center' aria-labelledby="tech-stack-heading">
 			<h3 id="tech-stack-heading" className="sr-only">Tecnologias e Habilidades</h3>
-			<ul className="grid grid-cols-4 md:grid-cols-6 gap-1" role="list" aria-label="Lista de tecnologias">
+			<ul className="grid grid-cols-3 md:grid-cols-6 gap-1" role="list" aria-label="Lista de tecnologias">
 				{TECH_STACK.map((item, index) => {
 					return (
 						<li key={index}>
